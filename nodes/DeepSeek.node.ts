6b054b01512d378dad051b4832306471cfe574fe
rubickecho <tangliangcheng852@gmail.com>
2024-07-29 19:19:23 +0800

    feat: init porject and add deepseek node

---
+++ nodes/DeepSeek.node.ts
@@ -0,0 +1,48 @@
+import type { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import { chatFields, chatOperations } from './ChatDescription';
+
+export class DeepSeek implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'DeepSeek',
+		name: 'deepSeek',
+		hidden: true,
+		icon: { light: 'file:DeepSeek.svg', dark: 'file:DeepSeek.svg' },
+		group: ['transform'],
+		version: [1, 1.1],
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Consume DeepSeek AI',
+		defaults: {
+			name: 'DeepSeek',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'deepSeekApi',
+				required: true,
+			},
+		],
+		requestDefaults: {
+			ignoreHttpStatusErrors: true,
+			baseURL: 'https://api.deepseek.com',
+		},
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Chat',
+						value: 'chat',
+					}
+				],
+				default: 'text',
+			},
+
+			...chatOperations,
+			...chatFields
+		],
+	};
+}
